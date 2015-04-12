48a24d0732a2b1ed934d3cc74dcc101a6b7fa616
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2015-04-13 00:23:27 +0900

    静的ページおよびテストの作成

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,4 +7,18 @@ describe "StaticPages" do
       expect(page).to have_content('Sample App')
     end
   end
+
+  describe "Help Page" do
+    it "Should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
 end
