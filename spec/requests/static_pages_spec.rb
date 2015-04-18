2da2ed4e72ef44181aab2c95dcac1daa43d19640
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2015-04-18 22:52:08 +0900

    ルーティング修正

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,53 +6,53 @@ describe "StaticPages" do
 
   describe "Home Page" do
     it "Should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the  title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("#{base_title}")
     end
     
     it "should have the  title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title(" | Home")
     end
   end
 
   describe "Help Page" do
     it "Should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the  title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the  title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
   describe "Contact page" do
     it "should have the content 'contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
 
     it "should have the  title 'Contect'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end
   end
