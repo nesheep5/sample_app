e309aa894f2a64a7cd752cc62252412869f79c22
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2015-04-28 23:33:58 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def create
-    @user = User.new(params[user_params])
+    @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
       
     end
   end
+
     private
 
     def user_params
