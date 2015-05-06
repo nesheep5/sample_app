baaca1e6e2d7b4297266d506c3ab9180b2bab531
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2015-05-06 19:48:13 +0900

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -17,7 +19,7 @@ class UsersController < ApplicationController
     end
   end
 
-    private
+  private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
