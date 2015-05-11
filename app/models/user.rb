bf7e06120ae6c96bc55bde8ecfde4bc3e74b5da9
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2015-05-11 09:08:30 +0900

    User破棄時、同時に関連するMicropostも破棄されるよう設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :microposts
+  #dependent: :destroy でmicropostも一緒に破棄することを指定 
+  has_many :microposts, dependent: :destroy
   
   before_save { email.downcase! } 
   before_create :create_remember_token
