2e5af2781f72d6a548ef27a37271b3b0c242d58f
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2015-05-20 09:28:39 +0900

    follower削除機能作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   #dependent: :destroy でmicropostも一緒に破棄することを指定 
   has_many :microposts, dependent: :destroy
-  
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
   before_save { email.downcase! } 
   before_create :create_remember_token
 
@@ -26,6 +27,18 @@ class User < ActiveRecord::Base
     # 完全な実装は第11章「ユーザーをフォローする」を参照してください。
     Micropost.where("user_id = ?", id)
   end
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end 
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end 
 
   private 
 
